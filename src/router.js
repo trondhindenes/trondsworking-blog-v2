0f518521195ed692da54b33b12e44f3ade74b25b
Trond Hindenes <trond@hindenes.com>
2020-01-10 00:48:52 +0100

    Optional camefrom route

--- src/router.js
+++ src/router.js
@@ -12,7 +12,7 @@ Vue.use(Router);
 export default new Router({
     routes: [
         {
-            path: "/",
+            path: "/:cameFrom?",
             name: 'index',
             component: Index,
             props: route => ({ cameFrom: route.params.cameFrom })
