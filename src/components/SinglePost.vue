9986640abec66d42f6f6e6a4e47e301988f4cbf0
Trond Hindenes <trond@hindenes.com>
2020-01-10 00:06:56 +0100

    Handle posts

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="card" v-if="post">
     <h2>{{ (post.title).toUpperCase() }}</h2>
-    <img v-if="post.mainImage" 
-        :src="imageUrlFor( post.mainImage ).ignoreImageParams()"
-    />
+    <img v-if="post.mainImage" :src="imageUrlFor( post.mainImage ).ignoreImageParams().width(calculateImageWidth)" />
     <vue-markdown v-for="blob in post.body" v-bind:key="blob._id">{{ blob.markdown }}</vue-markdown>
   </div>
 </template>
@@ -22,7 +20,12 @@ export default {
   props: ["id"],
   data() {
     return {
-      post: null
+      post: null,
+      window: {
+        width: 0,
+        height: 0,
+        imageWidth: 0
+      }
     };
   },
   methods: {
@@ -32,10 +35,50 @@ export default {
     imageUrlFor(source) {
       let imageBuilder = imageUrlBuilder(sanity);
       return imageBuilder.image(source);
+    },
+    compareSizes(size, ref) {
+      //only trigger resize if size diff is big,
+      //so we don't do excessive image gets against sanity.io
+      if (ref - size > 100) {
+        return true;
+      }
+      if (ref - size < -100) {
+        return true;
+      }
+      return false;
+    },
+    handleResize() {
+      let clientHeight = document.documentElement.clientHeight;
+      this.$log.debug("clientHeight", clientHeight);
+      if (this.compareSizes(clientHeight, this.window.height)) {
+        this.window.height = clientHeight;
+      }
+
+      let clientWidth = document.documentElement.clientWidth;
+      this.$log.debug("clientWidth", clientWidth);
+      if (this.compareSizes(clientWidth, this.window.width)) {
+        this.window.width = clientWidth;
+      }
     }
   },
   async created() {
     await this.fetchData();
+    window.addEventListener("resize", this.handleResize);
+    this.handleResize();
+  },
+  computed: {
+    calculateImageWidth: function() {
+      let imageWidth = this.window.width;
+      if (imageWidth > 700) {
+        imageWidth = 700;
+      } else {
+        imageWidth = imageWidth - 100;
+      }
+      return imageWidth;
+    }
+  },
+  watch: {
+    fetchData: "fetchData"
   }
 };
 </script>
